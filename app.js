1d1d492ea3f236627d35e8ab3844ced814bdf0ba
Shreya Khatry <shreyakhatry@Shreyas-MacBook-Air.local>
2023-12-13 22:50:55 +0530

    Complete TODO app in react and Node JS

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const mongoose=require("mongoose");
+
+const app=express();
+
+
+//connect mongoDB
+mongoose.connect("mongodb://localhost/todo_express",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+});
+
+//middlewares
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+
+//routes
+app.use(require("./routes/index"));
+app.use(require("./routes/todo"));
+
+//server configuration
+app.listen(3000,()=>console.log("Server started listening on port: 3000"));
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router()
+const Todo=require("../models/Todos");
+
+
+
+// routes will be here....
+router.get("/",async(req,res)=>{
+    const allTodo=await Todo.find()
+    res.render("index",{todo:allTodo})
+})
+
+
+module.exports=router;
