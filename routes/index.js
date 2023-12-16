8721fa63099110b58eb52185befc8c6c5cd05217
Shreya Khatry <shreyakhatry@Shreyas-MacBook-Air.local>
2023-12-16 19:02:08 +0530

    FInal commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 const router = require("express").Router()
 const Todo=require("../models/Todos");
 
-
-
 // routes will be here....
 router.get("/",async(req,res)=>{
     const allTodo=await Todo.find()
     res.render("index",{todo:allTodo})
 })
-
 
 module.exports=router;
